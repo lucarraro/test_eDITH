0f7bb8cf7f098d85259aec17566370d336a24541
Carraro <luca.carraro@eawag.ch>
2023-06-14 17:36:06 +0200

    small example

---
+++ example_eDITH.R
@@ -0,0 +1,53 @@
+rm(list=ls())
+
+library(rivnet)
+library(terra)
+library(LaplacesDemon)
+library(BayesianTools)
+
+river <- extract_river(outlet=c(637478,237413),
+                    EPSG=21781, #CH1903/LV03 coordinate system
+                    ext=c(6.2e5,6.6e5,2e5,2.5e5),
+                    z=9)
+
+river <- aggregate_river(river, maxReachLength = 2500)
+hydrodata <- data.frame(data=c(8, 15), type=c("w","Q"), node=river$AG$outlet*c(1,1))
+river <- hydro_river(hydrodata, river)
+
+r1 <- rast(system.file("extdata/landcover.tif", package="rivnet"))
+river <- covariate_river(r1, river)
+
+ss <- sort(river$AG$A, index.return=T); ss <- ss$ix
+samplingSites <- c(2,15,30,78,97,117,132,106,138,153,156,159,263,176,
+  215,189,11,70,79,87,45,209,26,213) # don't touch!
+
+tau <- 3*3600
+log_p0 <- -16
+
+covariates <- data.frame(urban=river$SC$locCov$landcover_1,
+                         elev=river$AG$Z,
+                         X=river$AG$X)
+for (i in 1:length(covariates)) covariates[,i] <- (covariates[,i]-mean(covariates[,i]))/sd(covariates[,i])
+
+param <- numeric(5)
+param <- c(tau, log_p0, -2, -1, 0.5)
+names(param) <- c("tau","log_p0","beta_urban","beta_Z","beta_X")
+
+p <- eDITH:::eval.p(param, covariates)
+C <- eDITH:::evalConc2_cpp(river, ss, river$AG$leng*river$AG$width, tau, p, "AG")
+
+set.seed(1)
+C_obs <- rnorm(length(samplingSites), C[samplingSites], 1e-13)
+
+dd <- data.frame(ID=samplingSites, values=C_obs)
+
+covariates <- data.frame(urban=river$SC$locCov$landcover_1,
+                         agriculture=river$SC$locCov$landcover_2,
+                         forest=river$SC$locCov$landcover_3,
+                         elev=river$AG$Z,
+                         log_drainageArea=log(river$AG$A))
+
+out <- eDITH::run_eDITH_BT(dd, river, covariates)
+
+out2 <- eDITH::run_eDITH_BT(dd, river) # works but doesn't converge
+
